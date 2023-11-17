f14523861d1d6990e66dc348058b943bc95ab651
danet <94226360+danetreab@users.noreply.github.com>
2023-11-17 14:00:37 +0700

    Update authConfig.ts

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -2,9 +2,9 @@ import { Configuration, LogLevel } from "@azure/msal-browser";
 
 export const ssoConfig: Configuration = {
   auth: {
-    clientId: "bac0c87e-6751-4546-97fe-8cb1d4554197",
+    clientId: "bac0c87e....",
     authority:
-      "https://login.microsoftonline.com/1e9461ec-5362-4329-ae46-61fa3e91c6d2",
+      "https://login.microsoftonline.com/tenentID",
     redirectUri: "http://localhost:5173/",
   },
   cache: {
